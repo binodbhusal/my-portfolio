bab3de7992ccdad24b466d2502b4b7b9937d1925
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-10-21 12:02:54 +0100

    Add social media component

---
+++ src/components/SocialMediaIcons.jsx
@@ -0,0 +1,42 @@
+import {
+  FaLinkedin, FaGithub, FaTwitter, FaMedium,
+} from 'react-icons/fa';
+
+const SocialMediaIcons = () => (
+  <div className="flex justify-center md:justify-start my-10 gap-7">
+    <a
+      className="hover:opacity-50 transition duration-500"
+      href="https://www.linkedin.com/in/binodbhusal/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FaLinkedin size={32} />
+    </a>
+    <a
+      className="hover:opacity-50 transition duration-500"
+      href="https://github.com/binodbhusal"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FaGithub size={32} />
+    </a>
+    <a
+      className="hover:opacity-50 transition duration-500"
+      href="https://twitter.com/Binod_ironLad"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FaTwitter size={32} />
+    </a>
+    <a
+      className="hover:opacity-50 transition duration-500"
+      href="https://medium.com/@binodreal1982"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <FaMedium size={32} />
+    </a>
+  </div>
+);
+
+export default SocialMediaIcons;
