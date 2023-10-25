77d48f5e5b456f62e84a2c5932b97aefc22cb00f
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-10-25 17:11:06 +0100

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,32 +5,42 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        blue: '#2cbce9',
-        red: '#dc4492',
-        yellow: '#fdcc49',
-        grey: '#ededed',
         'deep-blue': '#010026',
+        blue: '#2CBCE9',
+        red: '#DC4492',
+        yellow: '#FDCC49',
+        grey: '#ededed',
         'dark-grey': '#757575',
         'opaque-black': 'rgba(0,0,0,0.35)',
       },
-      backgroundImage: () => ({
+      backgroundImage: (theme) => ({
         'gradient-rainbow':
-        'linear-gradient(81.66deg, #00b5ee 7.21%, #ff445a4 45.05%, #ffba00 78.07%)',
+          'linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)',
+
         'gradient-rainblue':
-        'linear-gradient(90.66deg, #24cbff 14.53%, #fc59ff 69.36%, #ffbd0c 117.73%)',
+          'linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)',
       }),
       fontFamily: {
         playfair: ['Playfair Display', 'serif'],
         opensans: ['Open Sans', 'sans-serif'],
       },
       content: {
-        brush: 'url("./assets/brush.png")',
+        brush: "url('./image/brush.png')",
+        person1: "url('./assets/person-1.png')",
+        person2: "url('./assets/person-2.png')",
+        person3: "url('./assets/person-3.png')",
       },
     },
     screens: {
       xs: '480px',
-      sm: '760px',
+      ss: '620px',
+      sm: '768px',
       md: '1060px',
+      lg: '1200px',
+      xl: '1700px',
+    },
+    backdropFilter: {
+      blur: 'blur(5px)',
     },
   },
   plugins: [],
